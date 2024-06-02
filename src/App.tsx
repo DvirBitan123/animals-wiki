60e82fb480b28f4e02530781653a79dc126ba01d
Dvir Bitan <dvirbitan123455@gmail.com>
2024-06-02 15:38:39 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className='text-[100px] bg-blue-300'>
+      Reput Yadon
+    </div>
+  )
+}
+
+export default App
---
+++ src/Router/router.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+
+const Router = () => {
+  return (
+    <Routes>
+        {/* <Route index element={<HomePage />} /> */}
+      {/* <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+      </Route> */}
+    </Routes>
+  );
+};
